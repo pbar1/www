35f18e95c0ca014db24ac44c994a8f1234e02d4f
Pierce Bartine <piercebartine@gmail.com>
2023-07-24 01:12:49 -0700

    Redo pbar.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pbar1.github.io/www/",
+  site: "https://pbar.dev/",
   integrations: [
     tailwind({
       config: {
